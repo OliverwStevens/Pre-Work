9e6fb88a596861aa03e4e78b411d6493721d9f85
Oliver Stevens <oliverwstevens@gmail.com>
2025-02-21 10:00:39 -0500

    Wrote human and computer choice methods.

--- Foundations/rock-paper-scissors/rock_paper_scissors.js
+++ Foundations/rock-paper-scissors/rock_paper_scissors.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice == 0){
+        return "rock";
+    }
+    else if (choice == 1){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    return (prompt("Rock, paper, or scissors?")).toLowerCase();
+}
+
