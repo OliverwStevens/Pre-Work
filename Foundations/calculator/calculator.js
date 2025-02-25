ad7a7e978d376b05b43f23c2e524171a23396c8a
Oliver Stevens <oliverwstevens@gmail.com>
2025-02-24 21:13:41 -0500

    Linked html digit buttons to js

--- Foundations/calculator/calculator.js
+++ Foundations/calculator/calculator.js
@@ -16,5 +16,27 @@ function divide(a, b){
 function operate(operation, a, b){
     return operation(a, b);
 }
-console.log("hello");
 console.log(operate(add, 5, 4));
+
+const display = document.querySelector("#display");
+
+document.getElementById("btn1").addEventListener("click", () => handleClick("1"));
+document.getElementById("btn2").addEventListener("click", () => handleClick("2"));
+document.getElementById("btn3").addEventListener("click", () => handleClick("3"));
+document.getElementById("btn0").addEventListener("click", () => handleClick("0"));
+document.getElementById("btn4").addEventListener("click", () => handleClick("4"));
+document.getElementById("btn5").addEventListener("click", () => handleClick("5"));
+document.getElementById("btn6").addEventListener("click", () => handleClick("6"));
+document.getElementById("btnDot").addEventListener("click", () => handleClick("."));
+document.getElementById("btn7").addEventListener("click", () => handleClick("7"));
+document.getElementById("btn8").addEventListener("click", () => handleClick("8"));
+document.getElementById("btn9").addEventListener("click", () => handleClick("9"));
+document.getElementById("btnNeg").addEventListener("click", () => handleClick("(-)"));
+
+let calculation = "";
+function handleClick(buttonValue) {
+    console.log(`Button ${buttonValue} clicked!`);
+    
+    calculation += buttonValue;
+    display.innerHTML = calculation;
+}
