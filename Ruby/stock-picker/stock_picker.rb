5590174d994693d4a40a1056faa2099a458b9b81
Oliver Stevens <oliverwstevens@gmail.com>
2025-02-27 14:43:23 -0500

    Refactored code to be more readable

--- Ruby/stock-picker/stock_picker.rb
+++ Ruby/stock-picker/stock_picker.rb
@@ -1,18 +1,20 @@
 def stock_picker(prices)
   best_value = 0
-  days = []
-  #loop over each index
-  prices.each_with_index do |price, index|
-    #then loop over each index comparing for the best value in relation to the prior days
-    index.times do |sub_index|
-      value = price - prices[sub_index]
-      if (value > best_value)
-        best_value = value
-        days = [sub_index, index]
+  best_buy_day = 0
+  best_sell_day = 0
+
+  prices.each_with_index do |sell_price, sell_day|
+    (0...sell_day).each do |buy_day|
+      profit = sell_price - prices[buy_day]
+      if profit > best_value
+        best_value = profit
+        best_buy_day = buy_day
+        best_sell_day = sell_day
       end
     end
   end
-  puts(days)
+
+  [best_buy_day, best_sell_day]
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+puts stock_picker([17,3,6,9,15,8,6,1,10])
