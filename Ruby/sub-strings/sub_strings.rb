90a5a3402ab8218c8fa0bdaf08708b09649eff6a
Oliver Stevens <oliverwstevens@gmail.com>
2025-02-27 13:36:00 -0500

    Completed sub strings project.

--- Ruby/sub-strings/sub_strings.rb
+++ Ruby/sub-strings/sub_strings.rb
@@ -1,5 +1,19 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+def substrings(word, dictionary)
+  dictionary = dictionary.map(&:downcase)
+  word.downcase!
 
-def substrings (word, dictionary)
-  
+  substrings_found = {}
+
+  dictionary.each do |term|
+    count = word.scan(term).size
+
+    if count > 0
+      substrings_found[term] = count
+    end
+  end
+
+  puts substrings_found
 end
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
