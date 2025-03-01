ffbce4119030cd25f55799504b5d3f21eb245bd2
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-01 14:18:17 -0500

    Wrote  make_guess function

--- Ruby/mastermind/mastermind.rb
+++ Ruby/mastermind/mastermind.rb
@@ -8,6 +8,17 @@ def create_code(code, colors)
   end
 end
 
-create_code(code, colors)
+def make_guess(colors)
+  guess = []
+  4.times do |num|
+    puts "What is in postition #{num + 1}?"
+    # puts "Colors:"
+    puts colors.join(" ")
+    guess.push(gets.chomp)
+  end
+  guess
+end
 
+create_code(code, colors)
+p make_guess(colors)
 p code
