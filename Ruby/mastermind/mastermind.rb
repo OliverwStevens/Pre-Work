5be8b4e443877140cc93ad2e5ce46fc1f1fc9592
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-01 21:27:30 -0500

    Created computer bot and used temp arrays to handle duplicate colors

--- Ruby/mastermind/mastermind.rb
+++ Ruby/mastermind/mastermind.rb
@@ -1,9 +1,11 @@
 colors = %w[blue orange green purple yellow pink]
 code = []
+computer_guess = [nil, nil, nil, nil]
 
 # Function to generate the secret code
 def create_code(code, colors)
   4.times { code.push(colors.sample) }
+  code
 end
 
 # Function to make a guess with validation
@@ -27,36 +29,79 @@ def make_guess(colors)
 end
 
 # Function to check the guess and provide feedback
-def check_code(guess, code)
+def check_code(guess, code, computer_guess)
   if guess == code
     puts "You win!"
     true
   else
     feedback = []
-    code.each_with_index do |color, index|
-      if color == guess[index]
-        feedback.push("red") # Correct color in correct position
-      elsif code.include?(guess[index])
+    temp_code = code.dup
+    temp_guess = guess.dup
+
+    # First check for exact matches (red pegs)
+    temp_code.each_with_index do |color, index|
+      next unless color == temp_guess[index]
+
+      feedback.push("red") # Correct color in correct position
+      computer_guess[index] = color if computer_guess
+      temp_code[index] = nil
+      temp_guess[index] = nil
+    end
+
+    # Then check for color matches (white pegs)
+    temp_guess.each_with_index do |color, index|
+      next if color.nil?
+
+      if temp_code.include?(color)
         feedback.push("white") # Correct color, wrong position
+        temp_code[temp_code.index(color)] = nil
       else
         feedback.push(" ") # Incorrect color
       end
     end
+
     feedback
   end
 end
 
-# Create a secret code
-create_code(code, colors)
-p code
-puts "Welcome to Mastermind! Try to guess the secret code!"
+def make_computer_guess(colors, computer_guess)
+  guess = []
+  4.times do |num|
+    if computer_guess[num].nil?
+      guess.push(colors.sample)
+    else
+      guess.push(computer_guess[num])
+    end
+  end
+
+  puts "Computer guesses: #{guess.join(', ')}"
+  guess
+end
+
+# Main game
+puts "Welcome to Mastermind! Try to guess the secret code or beat the computer!"
+puts "Type 'yes' if you want to guess or 'no' to make the code"
+human = gets.chomp.downcase == "yes"
+
+if human
+  code = create_code(code, colors)
+  puts "Secret code has been created!"
+else
+  puts "You'll create the code and the computer will try to guess it."
+  code = make_guess(colors)
+end
 
 # Game loop: Player has 10 attempts to guess
 10.times do |i|
   puts "\nAttempt ##{i + 1}:"
-  guess = make_guess(colors)
 
-  feedback = check_code(guess, code)
+  guess = if human
+            make_guess(colors)
+          else
+            make_computer_guess(colors, computer_guess)
+          end
+
+  feedback = check_code(guess, code, human ? nil : computer_guess)
 
   if feedback == true
     puts "Game over"
@@ -66,5 +111,11 @@ puts "Welcome to Mastermind! Try to guess the secret code!"
     red_count = feedback.count("red")
     white_count = feedback.count("white")
     puts "Feedback: #{red_count} red(s), #{white_count} white(s)"
+
+    # If all attempts are used up
+    if i == 9 && feedback != true
+      puts "Game over! You ran out of attempts."
+      puts "The secret code was: #{code.join(', ')}"
+    end
   end
 end
