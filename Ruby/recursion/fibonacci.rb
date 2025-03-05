c2144c43a9b7ee4f671fbf52ff34e3c92fcbb46f
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-05 14:26:31 -0500

    Wrote iterative and recursive fibonacci methods

---
+++ Ruby/recursion/fibonacci.rb
@@ -0,0 +1,21 @@
+def fibs(n)
+  return [] if n <= 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  fib_sequence = [0, 1]
+  (n - 2).times do
+    fib_sequence.push(fib_sequence[-1] + fib_sequence[-2])
+  end
+  fib_sequence
+end
+
+def fibs_rec(n, sequence = [0, 1])
+  return sequence[0...n] if n <= sequence.length
+
+  sequence.push(sequence[-1] + sequence[-2])
+  fibs_rec(n, sequence)
+end
+
+puts fibs(8)
+puts fibs_rec(8)
