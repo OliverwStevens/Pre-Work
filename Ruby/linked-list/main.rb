7185559be672445102768a20bbf70ebdfd0f8c5c
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-06 15:05:16 -0500

    Updated main.rb for testing

--- Ruby/linked-list/main.rb
+++ Ruby/linked-list/main.rb
@@ -1,13 +1,12 @@
 require_relative "lib/linked_list"
 
 list = LinkedList.new
-list.append("Hello")
-list.append("Sir")
-list.prepend("Oh!")
 
-puts list
-puts list.find("Sir")
-list.insert_at("Good", 0)
-puts list
-list.remove_at(1)
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+
 puts list
