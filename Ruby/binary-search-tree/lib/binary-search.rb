91b48a7ee8206cde4d5ae8cbeef45813a77e4871
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-08 11:35:38 -0500

    Wrote deletion methods, fixed error in deleting leaf nodes

--- Ruby/binary-search-tree/lib/binary-search.rb
+++ Ruby/binary-search-tree/lib/binary-search.rb
@@ -9,16 +9,16 @@ class Node
 end
 
 class Tree
-  attr_reader :root
+  attr_accessor :root
 
   # accepts array of values
   def initialize(values)
-    @root = build_tree(values)
+    @root = build_tree(values.sort)
   end
 
   def build_tree(data)
     # remove duplicates
-    data.uniq!
+    data = data.uniq
     build_tree_recursive(data, 0, data.length - 1)
   end
 
@@ -50,11 +50,38 @@ class Tree
     root
   end
 
-  def get_successor(curr)
-    curr = curr.right
+  def get_successor(node)
+    current = node
+    current = current.left while !current.nil? && !current.left.nil?
+    current
+  end
 
-    curr = curr.left while !curr.nil? && !curr.left.nil?
-    curr
+  def delete(value)
+    @root = delete_node(@root, value)
+  end
+
+  def delete_node(root, value)
+    return root if root.nil?
+
+    if value < root.data
+      root.left = delete_node(root.left, value)
+    elsif value > root.data
+      root.right = delete_node(root.right, value)
+    else
+      if root.left.nil?
+        return root.right
+      elsif root.right.nil?
+        return root.left
+      end
+
+      successor = get_successor(root.right)
+
+      root.data = successor.data
+
+      root.right = delete_node(root.right, successor.data)
+    end
+
+    root
   end
 
   # printing function for visualization
--- Ruby/binary-search-tree/main.rb
+++ Ruby/binary-search-tree/main.rb
@@ -5,7 +5,6 @@ tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 puts tree.root.data
 tree.pretty_print
 
-tree.insert(97)
-tree.insert(2)
+tree.del_node(tree.root, 7)
 puts "Hello people"
 tree.pretty_print
