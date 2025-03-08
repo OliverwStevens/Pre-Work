aec055c43e2d01c43106caf3349a9225a83b7e09
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-08 11:39:06 -0500

    Fixed main.rb for delete method testing

--- Ruby/binary-search-tree/main.rb
+++ Ruby/binary-search-tree/main.rb
@@ -5,6 +5,6 @@ tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 puts tree.root.data
 tree.pretty_print
 
-tree.del_node(tree.root, 7)
+tree.delete(67)
 puts "Hello people"
 tree.pretty_print
