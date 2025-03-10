992f2dbf82e5e1bc38f8353108ba069446dd629a
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-10 14:35:57 -0400

    Used BFS iterative approach and created basic function with queue

--- Ruby/knight-travails/knight-travails.rb
+++ Ruby/knight-travails/knight-travails.rb
@@ -1,14 +1,35 @@
 CHESS_SQUARES = (0..7).flat_map { |x| (0..7).map { |y| [x, y] } }
 
-def knight_moves(start)
-  x, y = start
+def knight_moves(start, stop)
+  return [start] if start == stop
 
+  queue = [[start]]
+  visited = Set.new([start])
+
+  x, y = start
   moves = [
     [x + 2, y + 1], [x + 2, y - 1], [x - 2, y + 1], [x - 2, y - 1],
     [x + 1, y + 2], [x + 1, y - 2], [x - 1, y + 2], [x - 1, y - 2]
   ]
 
-  legal_moves = moves.select { |move| CHESS_SQUARES.include?(move) }
+  until queue.empty?
+    path = queue.shift
+    current = path.last
+
+    return path if current == stop
+
+    x, y = current
+    moves.each do |dx, dy|
+      next_move = [x + dx, y + dy]
+
+      if CHESS_SQUARES.include?(next_move) && !visited.include?(next_move)
+        visited.add(next_move)
+        queue.push(path + [next_move])
+      end
+    end
+  end
+
+  nil
 end
 
-p knight_moves([0, 0])
+p knight_moves([0, 0], [0, 1])
