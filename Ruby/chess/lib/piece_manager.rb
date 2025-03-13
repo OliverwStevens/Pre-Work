2e2b28b060c60d753c125b1663c41ddf9ac986ef
Oliver Stevens <oliverwstevens@gmail.com>
2025-03-13 11:12:46 -0400

    Fixed typos in coords arguments

--- Ruby/chess/lib/piece_manager.rb
+++ Ruby/chess/lib/piece_manager.rb
@@ -3,6 +3,7 @@ require_relative "rook"
 require_relative "knight"
 require_relative "bishop"
 require_relative "queen"
+require_relative "king"
 class Piece_Manager
   pieces = []
   8.times do |num|
@@ -21,19 +22,24 @@ class Piece_Manager
 
   # knights
   pieces.push(Knight.new("white", [1, 0]))
-  pieces.push(Knight.new("white", [1, 0]))
-  pieces.push(Knight.new("black", [6, 7]))
+  pieces.push(Knight.new("white", [6, 0]))
+  pieces.push(Knight.new("black", [1, 7]))
   pieces.push(Knight.new("black", [6, 7]))
 
   # bishops
   pieces.push(Bishop.new("white", [2, 0]))
-  pieces.push(Bishop.new("white", [2, 0]))
-  pieces.push(Bishop.new("black", [5, 7]))
+  pieces.push(Bishop.new("white", [5, 0]))
+  pieces.push(Bishop.new("black", [2, 7]))
   pieces.push(Bishop.new("black", [5, 7]))
 
   # queens
   pieces.push(Queen.new("white", [3, 0]))
   pieces.push(Queen.new("black", [3, 7]))
 
+  # kings
+  pieces.push(King.new("white", [4, 0]))
+  pieces.push(King.new("black", [4, 7]))
+
   pieces.each { |piece| p [piece.color, piece.icon, piece.coords] }
+  puts pieces.length
 end
